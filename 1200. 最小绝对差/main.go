5ec8ed07e3feb6b808a89490c2beb5b0dbb5ce5b
king <836334258@qq.com>
2021-01-13 20:00:42 +0800

    最小绝对差

--- 1200. 最小绝对差/main.go
+++ 1200. 最小绝对差/main.go
@@ -5,22 +5,26 @@ import (
 	"sort"
 )
 
-func main() {
-	arr := []int{3,8,-10,23,19,-4,-14,27}
+func minimumAbsDifference(arr []int) [][]int {
 	sort.Ints(arr)
-	left := 0
-	right := 1
-	c := arr[right] - left
-	var res [100][2]int
-	for right < len(arr) {
-		k := arr[right] - arr[left]
-		if k < c {
-			res[left][0] = arr[left]
-			res[left][1] = arr[right]
+	fmt.Println(arr)
+	var aa [][]int
+
+	min := arr[1] - arr[0]
+	for i := 0; i < len(arr)-1; i++ {
+		if min == arr[i+1]-arr[i] {
+			aa = append(aa, []int{arr[i], arr[i+1]})
+		} else if min > arr[i+1]-arr[i] {
+			min = arr[i+1] - arr[i]
+			aa = [][]int{}
+			aa = append(aa, []int{arr[i], arr[i+1]})
 		}
-		left++
-		right++
 	}
 
-	fmt.Println(res)
+	return aa
+}
+
+func main() {
+	aa := []int{40, 11, 26, 27, -20}
+	fmt.Println(minimumAbsDifference(aa))
 }
