46a4d7b05ccf9240e2b47237761a0f7b7186985e
king <836334258@qq.com>
2021-01-14 15:51:35 +0800

    Revert "a"
    This reverts commit 3593e454a249b2621e98974949394400c3c59aad.

--- 349. 两个数组的交集/main.go
+++ 349. 两个数组的交集/main.go
@@ -3,27 +3,11 @@ package main
 import "fmt"
 
 func intersection(nums1 []int, nums2 []int) []int {
-	var tmp []int
-	for i := 0; i < len(nums1); i++ {
-		for k := 0; k < len(nums2); k++ {
-			if nums1[i] == nums2[k] {
-				sign := 0
-				for _, v := range tmp {
-					if v == nums1[i] {
-						sign = 1
-					}
-				}
-				if sign == 0 {
-					tmp = append(tmp, nums1[i])
-				}
-			}
-		}
-	}
-	return tmp
+	return  nums1 & nums2
 }
 
-func main() {
-	nums1 := []int{4,9,5}
-	nums2 := []int{9,4,9,8,4}
-	fmt.Println(intersection(nums1, nums2))
+func main()  {
+	nums1:=[]int{1,2,2,1}
+	nums2:=[]int{2,2}
+	fmt.Println(intersection(nums1,nums2))
 }
