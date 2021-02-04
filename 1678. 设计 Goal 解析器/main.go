a5cce728a2d2c4d1110143f09c8c58660f32339d
king <836334258@qq.com>
2021-02-04 16:45:25 +0800

    1678

---
+++ 1678. 设计 Goal 解析器/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func interpret(command string) string {
+	command = strings.ReplaceAll(command, "G", "G")
+	command = strings.ReplaceAll(command, "()", "o")
+	command = strings.ReplaceAll(command, "(al)", "al")
+	return command
+
+}
+
+func main() {
+	fmt.Println(interpret("G()()()()(al)"))
+}
